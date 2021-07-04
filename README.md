3601d5ea3ee0b30f142e93feb8e2159a2fa491e4
ananarchie <anastasia.bogdanova.18@gmail.com>
2021-07-04 19:59:58 -0400

    Refactor + minimal logic changes

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# purge
-# run the script in authorize.txt first
-# i recommend running it one line at a time in an IDE or CMD window
-# you will need to manually authorize the app in a browser window and then continue running the code after that is done
-# once you have authorized and received oauth tokens with elevated permissions you can continue on to the purge script
+# PurgeScript
+
+A script to reduce your follower count on Tumblr by softblocking some non-mutual followers.
+
+## Dependencies
+
+Dependencies:
+* [requests-oauthlib](https://pypi.org/project/requests-oauthlib/)
+* [ratelimit](https://pypi.org/project/ratelimit/)
+
+## Authorization
+
+This is a real pain.
+1. Go [here](https://www.tumblr.com/oauth/apps) to "register" an app. The name, website & description can be anything, these values don't matter. For the default callback URL you can set, for example, `https://www.tumblr.com/dashboard`
+2. Save your **OAuth Consumer Key** (used for `client_key`) and **Secret Key** (obtained by clicking "Show secret key" & used for `client_secret`).
+3. Use these as parameters when running the script.
+
+## Usage
+
+Use it like this:
+```shell
+python purge.py -u <your_username> -m <max_followers_to_pull> -k <client_key> -s <client_secret>
+```
+
+**Note:** When you specify the max number of followers to pull, this is the number of recent followers that is checked, among which non-mutuals are blocked, NOT necessarily the number of users that will be blocked.
--- purge.py
+++ purge.py
@@ -1,108 +1,159 @@
-# purge
+#!/bin/env python
 
 # import libraries
+import sys, getopt
+import webbrowser
+import json
+
 import requests
 from requests_oauthlib import OAuth1Session
 from requests_oauthlib import OAuth1
 
-# authenticate
-client_key = 'enter client key here'
-client_secret = 'enter client secret here'
+# import this library to set rate limit	
+from ratelimit import limits
 
-# enter the oauth token and secret you received from the authorize script
-oauth_tokens = {'oauth_token': 'enter oauth token here', 'oauth_token_secret': 'enter oauth token secret here'}
+# global vars - to avoid having a billion params everywhere
+username = ''
+max_to_block = 0
+client_key = ''
+client_secret = ''
+resource_owner_key = ''
+resource_owner_secret = ''
+non_mutuals = []
 
-# set oauth credentials to these variables for query signing
-resource_owner_key = oauth_tokens.get('oauth_token')
-resource_owner_secret = oauth_tokens.get('oauth_token_secret')
+# useful urls
+base_url = 'http://api.tumblr.com/v2/blog/' + username + '.tumblr.com/'
+blocks = base_url + 'blocks'
+followers_url = base_url + 'followers'
 
-# set uri for blocking
-blocks = 'http://api.tumblr.com/v2/blog/yoururl.tumblr.com/blocks'
+# authorize
+def authorize():
+    request_token_url = 'https://www.tumblr.com/oauth/request_token'
+    base_authorization_url = 'https://www.tumblr.com/oauth/authorize'
 
-# query signing
-queryoauth = OAuth1(client_key, client_secret,
-                    resource_owner_key, resource_owner_secret,
-                    signature_type='query')
+    oauth = OAuth1Session(client_key, client_secret=client_secret)
+    fetch_response = oauth.fetch_request_token(request_token_url)
 
-# set uri for retrieving list of your followers
-followers_url = 'http://api.tumblr.com/v2/blog/yoururl.tumblr.com/followers'
+    resource_owner_key = fetch_response.get('oauth_token')
+    resource_owner_secret = fetch_response.get('oauth_token_secret')
 
-# returns a JSON object containing your followers
-followers = requests.get(followers_url, auth=queryoauth)
+    authorization_url = oauth.authorization_url(base_authorization_url)
+    webbrowser.open(authorization_url)
 
-# convert JSON object to a standard python list
-import json
-followers = json.loads(followers.text)
+    redirect_response = input('Paste the full redirect URL here: ')
+    oauth_response = oauth.parse_authorization_response(redirect_response)
 
-# remove metadata at the top of the list
-followers = followers['response']['users']
+    verifier = oauth_response.get('oauth_verifier')
 
-# followers should now contain a list of dictionaries
+    access_token_url = 'https://www.tumblr.com/oauth/access_token'
+    oauth = OAuth1Session(client_key,
+        client_secret=client_secret,
+        resource_owner_key=resource_owner_key,
+        resource_owner_secret=resource_owner_secret,
+        verifier=verifier)
+
+def add_followers(followers, offset, limit):
+    followers_to_add = requests.get(followers_url, params = {"offset": offset}, auth = queryoauth)
+    followers_to_add = json.loads(followers_to_add.text)
+    followers_to_add = followers_to_add['response']['users']
+    followers.extend(followers_to_add)
+
+# after the following function is run, followers should contain a list of dictionaries
 # each dictionary item is a follower
 # we can only get about 20 followers at a time though
 # so to block more people we will need to create additional lists
 # using the offset parameter we can make a GET request starting at the 20th follower
 # then the 40th, 60th, etc. and then we can append those lists to the first follower list
+def build_followers_list(queryoauth):
+    followers = requests.get(followers_url, auth=queryoauth)
+    followers = json.loads(followers.text)
+    followers = followers['response']['users']
 
-followers2 = requests.get(followers_url, params = {"offset": 20}, auth = queryoauth)
-followers2 = json.loads(followers2.text)
-followers2 = followers2['response']['users']
-followers.extend(followers2)
+    followers = []
+    range_max = int(max_to_block // 20)
+    last_request_limit = 0
 
-followers3 = requests.get(followers_url, params = {"offset": 40}, auth = queryoauth)
-followers3 = json.loads(followers3.text)
-followers3 = followers3['response']['users']
-followers.extend(followers3)
+    if range_max * 20 < max_to_block:
+        range_max += 1
+        last_request_limit = range_max * 20 - max_to_block
 
-followers4 = requests.get(followers_url, params = {"offset": 60}, auth = queryoauth)
-followers4 = json.loads(followers4.text)
-followers4 = followers4['response']['users']
-followers.extend(followers4)
+    for i in range(1, range_max):
+        print(i * 20)
+        offset = i * 20
+        limit = 20
 
-followers5 = requests.get(followers_url, params = {"offset": 80}, auth = queryoauth)
-followers5 = json.loads(followers5.text)
-followers5 = followers5['response']['users']
-followers.extend(followers5)
+        if i == range_max:
+            limit = last_request_limit
 
-followers6 = requests.get(followers_url, params = {"offset": 100}, auth = queryoauth)
-followers6 = json.loads(followers6.text)
-followers6 = followers6['response']['users']
-followers.extend(followers6)
-
-followers7 = requests.get(followers_url, params = {"offset": 120}, auth = queryoauth)
-followers7 = json.loads(followers7.text)
-followers7 = followers7['response']['users']
-followers.extend(followers7)
-
-followers8 = requests.get(followers_url, params = {"offset": 140}, auth = queryoauth)
-followers8 = json.loads(followers8.text)
-followers8 = followers8['response']['users']
-followers.extend(followers8)
-
-# create a new list that does not contain any mutuals
-non_mutuals = [i for i in followers if not (i['following'] == True)]
+        add_followers(followers, offset, limit)
+    
+    # create a new list that does not contain any mutuals
+    global non_mutuals
+    non_mutuals = [i for i in followers if not (i['following'] == True)]
 
 # define a function that will be used to softblock every follower in non_mutuals		
 def softblock(x):
-	block = requests.post(blocks, data=x, auth=queryoauth)
-	unblock = requests.delete(blocks, data=x, auth=queryoauth)
-	if block.status_code == 201 and unblock.status_code == 200:
-		return 'success'
-	else:
-		return 'failed'
+    block = requests.post(blocks, data=x, auth=queryoauth)
+    unblock = requests.delete(blocks, data=x, auth=queryoauth)
+    if block.status_code == 201 and unblock.status_code == 200:
+        return 'success'
+    else:
+        return 'failed'
 
-# import this library to set rate limit	
-from ratelimit import limits
+@limits(calls=1, period=1)
+def purge():
+    for i in range(len(non_mutuals)):
+        blog = {'blocked_tumblelog': non_mutuals[i]['name']}
+        print(blog)
+        softblock(blog)
 
+# define a function that will be used to softblock every follower in non_mutuals
 # tumblr api block and unblock calls are rate limited at 60 requests per minute
 # decorator sets limit of 1 call per second, equivalent to 60 calls per minute
 # purge() function will iterate through each item in the list, declare a variable containing the parameters needed for the block and unblock calls
 # then call softblock() function
-@limits(calls=1, period=1)
 def purge():
-	for i in range(len(non_mutuals)):
-		blog = {'blocked_tumblelog': non_mutuals[i]['name']}
-		softblock(blog)
+    # query signing & call build followers list function
+    queryoauth = OAuth1(client_key, client_secret,
+                    resource_owner_key, resource_owner_secret,
+                    signature_type='query')
 
-#finally, call your function
-purge()
+    build_followers_list(queryoauth)
+
+def main():
+    long_args = ["help", "username=", "key=", "secret=", "max="]
+    global username, max_to_block, client_key, client_secret
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "u:m:k:s:", long_args)
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    
+    for o, a in opts:
+        if o in ("-u", "--username"):
+            username = a
+        elif o in ("-m", "--max"):
+            max_to_block = int(a)
+            if not isinstance(max_to_block, int):
+                print("max to block must be an integer")
+                sys.exit(2)
+        elif o in ("-k", "--key"):
+            client_key = a
+        elif o in ("-s", "--secret"):
+            client_secret = a
+        else:
+            assert False, "unhandled option"
+
+    if not username or not max_to_block:
+        print("username and max followers to block required")
+        sys.exit(1)
+    if not client_key or not client_secret:
+        print("client key and secret required")
+        sys.exit(1)
+
+    authorize()
+    purge()
+
+if __name__ == "__main__":
+    main()
